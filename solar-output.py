a718efdab20ce3cfaf433d99e4ca0448f1163f71
mdv1973 <38200702+mdv1973@users.noreply.github.com>
2018-04-09 23:28:51 +0200

    Create solar-output.py

---
+++ solar-output.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+#config
+cfg = {
+  'ip_net': '10.0.0.0',
+  'mac': '00:00:00:00:00:00',
+  'db': { 'h': 'localhost', 'u': 'dbuser', 'p': 'pwd', 'd': 'dbname' },
+  'api-key': 'blah',
+}
+
+def _get_inverter_ip(ip_net, mac):
+  #TODO
+  inv_ip = '0.0.0.0'
+  return inv_ip
+
+def get_inverter_data(ip_net, mac):
+  #TODO
+  eg = pg = 0
+  return eg, pg
+
+def insert_in_db(eg, pg):
+  #TODO
+  return
+
+def post_pvoutput(eg, pg):
+  #TODO
+  return
+
+#main
+eg, pg = get_inverter_data(cfg['ip_net'], cfg['mac'])
+insert_in_db(cfg['db'], eg, pg)
+post_pvoutput(cfg['api_key'], eg, pg)
