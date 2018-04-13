270b4518fb601d934701cfe9455dfb3959ebdc0d
mdv1973 <38200702+mdv1973@users.noreply.github.com>
2018-04-13 17:27:34 +0200

    sleep mode config

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 # config file for solar-output.py
 cfg = {
+  'wake_hour': 8, # hour of day to start measurements
+  'sleep_hour': 21, # hour of day to stop measurements
   'ip_net': '192.168.0.0/24', #local IP subnet having inverter
   'mac': '00:90:A9', #prefix of (or full) inverter MAC address
   'db': {
