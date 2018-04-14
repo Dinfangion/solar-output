c6ca4d5a6668f32bc09b1d7056fb8476f717afe9
Dinfangion <25172314+Dinfangion@users.noreply.github.com>
2018-04-14 19:18:57 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # config file for solar-output.py
 cfg = {
-  'wake_hour': 8, # hour of day to start measurements
-  'sleep_hour': 21, # hour of day to stop measurements
+  'wake_hour': 6, # hour of day to start measurements
+  'sleep_hour': 23, # hour of day to stop measurements
   'ip_net': '192.168.0.0/24', #local IP subnet having inverter
-  'mac': '00:90:A9', #prefix of (or full) inverter MAC address
+  'mac': 'e0:02:02', #prefix of (or full) inverter MAC address
   'db': {
     'host': 'localhost', # mysql/mariadb server ip or hostname; leave empty to skip database
-    'user': 'root',
-    'pwd': '',
+    'user': 'solar_rw',
+    'pwd': 'yourpassword',
     'db_name': 'solar',
     'table': 'solar_output',
   },
